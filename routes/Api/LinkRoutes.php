0885ab7abad8e6a0fb571e8efd79da03d199f135
Aleksei-Brodiagin <aleksei.brodiagin@gmail.com>
2022-04-11 00:28:17 +0500

    fix: routes

--- routes/Api/LinkRoutes.php
+++ routes/Api/LinkRoutes.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\LinkController;
 
 Route::controller(LinkController::class)->name('link.')->group(function() {
     Route::post('/link', 'store')->name('store');
+    Route::get('/{code}', 'show')->name('show');
 });
--- routes/Web/LinkRoutes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\LinkController;
-
-Route::controller(LinkController::class)->name('link.')->group(function() {
-    Route::get('/{code}', 'show')->name('show');
-});

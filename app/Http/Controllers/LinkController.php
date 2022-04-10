351c858372c8797b8ed075ddc0052c5cac54cd39
Aleksei-Brodiagin <aleksei.brodiagin@gmail.com>
2022-04-10 19:17:21 +0500

    feat: add link controller and request

---
+++ app/Http/Controllers/LinkController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LinkCreateRequest;
+use Illuminate\Http\Request;
+
+class LinkController extends Controller
+{
+    public function show()
+    {
+
+    }
+
+    public function store(LinkCreateRequest $request)
+    {
+
+    }
+}
